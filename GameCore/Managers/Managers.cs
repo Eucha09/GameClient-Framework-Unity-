824c403f2551e423fad31ea6439b0dd0b7e42981
Eucha09 <euchan5791@naver.com>
2022-11-27 20:42:08 +0900

    update DataManager

--- GameCore/Managers/Managers.cs
+++ GameCore/Managers/Managers.cs
@@ -8,9 +8,9 @@ public class Managers : MonoBehaviour
     static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고온다
 
 	#region Contents
-	GameManagerEx _game = new GameManagerEx();
+	//GameManagerEx _game = new GameManagerEx();
 
-    public static GameManagerEx Game {  get { return Instance._game; } }
+    //public static GameManagerEx Game {  get { return Instance._game; } }
 	#endregion
 
 	#region Core
